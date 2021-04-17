a925872d16522193174aabe1c20ec3c38d9763ca
Boken-art <72617944+Boken-art@users.noreply.github.com>
2021-04-17 11:28:00 +0200

    AddProject

---
+++ Settings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Shool2
+{
+    class Settings
+    {
+        public string shoolConnString => ConfigurationManager.ConnectionStrings["SconnString"].ConnectionString;
+
+        
+    }
+}
