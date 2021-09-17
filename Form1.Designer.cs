f5267117343446a1c2627fec6b7ce7a62cd9f43a
Kirill Konovalov <rothmenk@gmail.com>
2021-09-17 19:01:57 +0200

    minor update

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -5,6 +5,7 @@
         /// <summary>
         /// Required designer variable.
         /// </summary>
+        /// 
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
@@ -34,6 +35,7 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
@@ -47,16 +49,15 @@
             this.panel3 = new System.Windows.Forms.Panel();
             this.button2 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.studentTableAdapter = new Shool2.DatabaseDataSetTableAdapters.StudentTableAdapter();
             this.tableAdapterManager = new Shool2.DatabaseDataSetTableAdapters.TableAdapterManager();
             nameLabel = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize) (this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize) (this.studentBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize) (this.databaseDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet)).BeginInit();
             this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -65,9 +66,10 @@
             nameLabel.AutoSize = true;
             nameLabel.BackColor = System.Drawing.Color.White;
             nameLabel.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            nameLabel.Location = new System.Drawing.Point(114, 198);
+            nameLabel.Location = new System.Drawing.Point(169, 72);
+            nameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             nameLabel.Name = "nameLabel";
-            nameLabel.Size = new System.Drawing.Size(63, 13);
+            nameLabel.Size = new System.Drawing.Size(94, 20);
             nameLabel.TabIndex = 17;
             nameLabel.Text = "Enter Name";
             nameLabel.Click += new System.EventHandler(this.nameLabel_Click);
@@ -75,9 +77,10 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Shool2.Properties.Resources.Avatar;
-            this.pictureBox1.Location = new System.Drawing.Point(30, 196);
+            this.pictureBox1.Location = new System.Drawing.Point(43, 69);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(49, 49);
+            this.pictureBox1.Size = new System.Drawing.Size(74, 75);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -85,10 +88,11 @@
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Image = ((System.Drawing.Image) (resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(30, 324);
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(43, 265);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(49, 45);
+            this.pictureBox2.Size = new System.Drawing.Size(74, 69);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
@@ -96,16 +100,32 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Fuchsia;
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(393, 559);
+            this.panel1.Size = new System.Drawing.Size(590, 860);
             this.panel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.button1.Location = new System.Drawing.Point(109, 755);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(408, 75);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Create User";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.sendText);
             // 
             // panel2
             // 
-            this.panel2.BackColor = System.Drawing.Color.Indigo;
+            this.panel2.BackColor = System.Drawing.Color.White;
             this.panel2.Controls.Add(this.radioButton2);
             this.panel2.Controls.Add(this.radioButton1);
             this.panel2.Controls.Add(nameLabel);
@@ -116,17 +136,19 @@
             this.panel2.Controls.Add(this.textBoxName);
             this.panel2.Controls.Add(this.pictureBox2);
             this.panel2.Controls.Add(this.pictureBox1);
-            this.panel2.Location = new System.Drawing.Point(12, 12);
+            this.panel2.Location = new System.Drawing.Point(40, 87);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(365, 535);
+            this.panel2.Size = new System.Drawing.Size(526, 643);
             this.panel2.TabIndex = 3;
             // 
             // radioButton2
             // 
-            this.radioButton2.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.radioButton2.Location = new System.Drawing.Point(72, 439);
+            this.radioButton2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.radioButton2.Location = new System.Drawing.Point(106, 442);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(104, 24);
+            this.radioButton2.Size = new System.Drawing.Size(156, 37);
             this.radioButton2.TabIndex = 21;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Teacher";
@@ -134,10 +156,11 @@
             // 
             // radioButton1
             // 
-            this.radioButton1.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.radioButton1.Location = new System.Drawing.Point(72, 409);
+            this.radioButton1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.radioButton1.Location = new System.Drawing.Point(106, 396);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(104, 24);
+            this.radioButton1.Size = new System.Drawing.Size(156, 37);
             this.radioButton1.TabIndex = 19;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Student";
@@ -149,18 +172,20 @@
             this.surename.AutoSize = true;
             this.surename.BackColor = System.Drawing.Color.White;
             this.surename.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.surename.Location = new System.Drawing.Point(93, 225);
+            this.surename.Location = new System.Drawing.Point(138, 113);
+            this.surename.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.surename.Name = "surename";
-            this.surename.Size = new System.Drawing.Size(83, 13);
+            this.surename.Size = new System.Drawing.Size(126, 20);
             this.surename.TabIndex = 17;
             this.surename.Text = "Enter Surename";
             this.surename.Click += new System.EventHandler(this.surename_Click);
             // 
             // textBoxSur
             // 
-            this.textBoxSur.Location = new System.Drawing.Point(191, 225);
+            this.textBoxSur.Location = new System.Drawing.Point(284, 113);
+            this.textBoxSur.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxSur.Name = "textBoxSur";
-            this.textBoxSur.Size = new System.Drawing.Size(139, 20);
+            this.textBoxSur.Size = new System.Drawing.Size(206, 26);
             this.textBoxSur.TabIndex = 16;
             this.textBoxSur.Click += new System.EventHandler(this.textBoxSur_TextChanged);
             this.textBoxSur.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
@@ -170,26 +195,29 @@
             this.pass.AutoSize = true;
             this.pass.BackColor = System.Drawing.Color.White;
             this.pass.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.pass.Location = new System.Drawing.Point(96, 343);
+            this.pass.Location = new System.Drawing.Point(142, 295);
+            this.pass.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.pass.Name = "pass";
-            this.pass.Size = new System.Drawing.Size(81, 13);
+            this.pass.Size = new System.Drawing.Size(121, 20);
             this.pass.TabIndex = 9;
             this.pass.Text = "Enter Password";
             this.pass.Click += new System.EventHandler(this.label3_Click);
             // 
             // textBoxPasword
             // 
-            this.textBoxPasword.Location = new System.Drawing.Point(191, 341);
+            this.textBoxPasword.Location = new System.Drawing.Point(284, 292);
+            this.textBoxPasword.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxPasword.Name = "textBoxPasword";
-            this.textBoxPasword.Size = new System.Drawing.Size(139, 20);
+            this.textBoxPasword.Size = new System.Drawing.Size(206, 26);
             this.textBoxPasword.TabIndex = 3;
             this.textBoxPasword.Click += new System.EventHandler(this.textBoxPasword_TextChanged);
             // 
             // textBoxName
             // 
-            this.textBoxName.Location = new System.Drawing.Point(191, 196);
+            this.textBoxName.Location = new System.Drawing.Point(284, 69);
+            this.textBoxName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxName.Name = "textBoxName";
-            this.textBoxName.Size = new System.Drawing.Size(139, 20);
+            this.textBoxName.Size = new System.Drawing.Size(206, 26);
             this.textBoxName.TabIndex = 2;
             this.textBoxName.Click += new System.EventHandler(this.textBoxName_TextChanged);
             this.textBoxName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
@@ -202,50 +230,43 @@
             // databaseDataSet
             // 
             this.databaseDataSet.DataSetName = "DatabaseDataSet";
-            this.databaseDataSet.Namespace = "http://tempuri.org/DatabaseDataSet.xsd";
             this.databaseDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // panel3
             // 
-            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (192)))), ((int) (((byte) (0)))), ((int) (((byte) (192)))));
+            this.panel3.BackColor = System.Drawing.Color.Fuchsia;
             this.panel3.Controls.Add(this.button2);
             this.panel3.Controls.Add(this.richTextBox1);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel3.Location = new System.Drawing.Point(393, 0);
+            this.panel3.Location = new System.Drawing.Point(590, 0);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(481, 492);
+            this.panel3.Size = new System.Drawing.Size(721, 860);
             this.panel3.TabIndex = 3;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(394, 11);
+            this.button2.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.button2.Location = new System.Drawing.Point(633, 14);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 35);
+            this.button2.Size = new System.Drawing.Size(75, 48);
             this.button2.TabIndex = 1;
             this.button2.Text = "Exit";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(26, 19);
+            this.richTextBox1.Location = new System.Drawing.Point(84, 87);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(338, 405);
+            this.richTextBox1.Size = new System.Drawing.Size(556, 643);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.Fuchsia;
-            this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(499, 498);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(272, 49);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Create User";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.sendText);
             // 
             // studentTableAdapter
             // 
@@ -259,25 +280,28 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(874, 559);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(1311, 860);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize) (this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize) (this.studentBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize) (this.databaseDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet)).EndInit();
             this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
+
         }
 
         private System.Windows.Forms.RadioButton radioButton2;
