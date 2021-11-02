4c33c51fd2d5e28b02b69aff52c43e9f436032d6
Boken-art <72617944+Boken-art@users.noreply.github.com>
2021-11-02 19:58:14 +0100

    New adds and debug ReadLine

--- Form1.cs
+++ Form1.cs
@@ -10,13 +10,18 @@ namespace Shool2
 {
     public partial class Form1 : MaterialSkin.Controls.MaterialForm
     {
+        #region Variables
         private Settings _settings { get => settings; set => settings = value; }
         private Settings settings;
         private readonly Random _random = new Random();
         private string label = "user";
-
+        private string logPath = @"C:\Users\hioli\OneDrive\Рабочий стол\output\";
+        private StringBuilder logger = new StringBuilder();
+        //private string pathFile = @"C:\Users\hioli\OneDrive\Рабочий стол\output\dragDrop\";
         private int min = 10000;
         private int max = 20000;
+        #endregion
+
         public Form1()
         {
             InitializeComponent();
@@ -63,7 +68,7 @@ namespace Shool2
 
         }
 
-
+        #region Elements
         private void AddWork()
         {
             //Work work1 = new Work(workname, studentid, predmetid, titul)
@@ -165,54 +170,7 @@ namespace Shool2
             label = "Teacher";
         }
 
-        private void materialButton1_Click(object sender, EventArgs e) //LOGIN
-        {
-            //Check login
-            string pathLocal = @"C:\Users\hioli\OneDrive\Рабочий стол\output\" + materialTextBox1.Text + ".txt";
-
-            try
-            {
-                File.ReadAllText(pathLocal);
-                var user_password = "";
-                using (StreamReader sr = File.OpenText(pathLocal))
-                {
-
-                    string s = "";
-                    //Check user file not empty
-                    while ((s = sr.ReadLine()) != null)
-                    {
-                        string[] items = s.Split('/');
-                        user_password = items[3];
-                        var user_type = items[2];
-                    }
-
-                    //Check password correct
-                    if (user_password != materialTextBox2.Text)
-                    {
-                        richTextBox1.AppendText("Password error  \n");
-                    }
-                }
-                richTextBox1.AppendText("Login successful \n"); //После всех действий - сообщение ОК
-            } catch (Exception ex)
-            {
-                richTextBox1.AppendText("Login error \n"); //Если логин не совпал  - сообщение Не ОК
-            }   //FIXME error with correct login
-
-
-        }
-
-        private void materialButton1_CreateWork(object sender, EventArgs e)
-        {
-
-            /*if (user_type == "student")
-            {
-                //create work Append (добавляем строку)
-            }
-            //TODO
-            */
-        }
-
-        private void materialTextBox1_TextChanged(object sender, EventArgs e)
+                private void materialTextBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
@@ -229,12 +187,76 @@ namespace Shool2
         {
 
         }
+        #endregion
+
+
+        private void materialButton1_Click(object sender, EventArgs e) //LOGIN
+        {
+            //Check login
+            string pathLocal = @"C:\Users\hioli\OneDrive\Рабочий стол\output\" + materialTextBox1.Text + ".txt";
+
+            try
+            {
+                File.ReadAllText(pathLocal);
+                var user_password = string.Empty;
+                using (StreamReader sr = File.OpenText(pathLocal))
+                {
+
+                    string s = string.Empty;
+                    //Check user file not empty
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        string[] items = s.Split('/');
+                        string elem = items[3];    // TODO looger разобраться
+                        user_password = elem;
+                        var user_type = items[2];
+                    }
+
+                    //Check password correct
+                    if (user_password != materialTextBox2.Text)
+                    {
+                        richTextBox1.AppendText("Password error  \n");
+                    }
+                }
+                richTextBox1.AppendText("Login successful \n"); //После всех действий - сообщение ОК
+            } 
+            catch (Exception ex)
+            {
+                logger.Append(ex);
+                File.AppendAllText(logPath + "log.txt", logger.ToString());  //out of bounds : 210
+                richTextBox1.AppendText("See log file \n");
+            }   
+
+
+        }
+
+        private void materialButton1_CreateWork(object sender, EventArgs e)
+        {
+
+            /*if (user_type == "student")
+            {
+                //create work Append (добавляем строку)
+            }
+            //TODO
+            */
+        }
+
 
         private void panel1_DragEnter(object sender, DragEventArgs e)
         {
+            e.Effect = DragDropEffects.All;
+            string pathLocal = @"C:\Users\rothm\Desktop\output\" + materialTextBox1.Text + ".txt";
+            richTextBox2.AppendText("Данные скопированы!");
 
-
-
+            if(File.Exists(pathLocal))
+            {
+                using(StreamWriter sw = File.AppendText(pathLocal))
+                {
+                    sw.WriteLine("work8;");
+                    sw.WriteLine("текст из файла");
+                    sw.WriteLine("end;");
+                }
+            }
 
 
             // Login -> D&D файла -> Считываем и добавляек к файлу юзера
@@ -242,17 +264,16 @@ namespace Shool2
 
         private void panel1_DragDrop(object sender, DragEventArgs e)
         {
+            //TODO Debug drop text
             string pathFile = @"C:\Users\hioli\OneDrive\Рабочий стол\output\dragDrop\";
             string[] fileGet = (string[])e.Data.GetData(DataFormats.FileDrop);
             string fileNewplace = fileGet[0];
+            string line = string.Empty;
             File.Create(fileNewplace);
 
             e.Effect = DragDropEffects.All;
             StringBuilder text = new StringBuilder();
 
-
-            //string path = @"";
-            string token = "@";
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
                 string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
@@ -269,9 +290,9 @@ namespace Shool2
                                 while (sr.Peek() >= 0)
                                 {   //TODO выбрать номер работы
 
-                                    text.Append(sr.ReadLine());
+                                    text.Append(line = sr.ReadLine());
 
-                                    if (string.Compare("end;", sr.ReadLine()) == 0)
+                                    if (string.Compare("end;", line) == 0)
                                     {
                                         break;
                                     }
@@ -284,13 +305,16 @@ namespace Shool2
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine(ex);   //TODO сохранять логи ошибок в файл
+                    logger.Append(ex);
+                    File.AppendAllText(logPath + "log.txt", logger.ToString());
+                    Console.WriteLine(ex);
                 }
 
 
 
             }
 
+            #region Comments
             /*using (OpenFileDialog dialog = new OpenFileDialog())  
             {  
                 if (dialog.ShowDialog() == DialogResult.OK)  
@@ -309,16 +333,17 @@ namespace Shool2
             //string[] files = e.Data.GetData(DataFormats.FileDrop) as string[];
             // if (files == null || files.Length == 0) return;
             //  textBox1.Text = files.First(); 
-
+            #endregion
         }
 
             private void button2_Click_1(object sender, EventArgs e)
             {
                 string path = @"C:\Users\hioli\OneDrive\Рабочий стол\output\" + materialTextBox1.Text + ".txt";
-                string logPath = @"C:\Users\hioli\OneDrive\Рабочий стол\output\log.txt";
                 StringBuilder text = new StringBuilder();
                 string loosePart = "";
-                //todo Why read dont work 
+                string line = "";
+            
+                
                 try 
                 {
                     File.ReadAllText(path);
@@ -328,17 +353,18 @@ namespace Shool2
                         {
                             if (string.Compare("work;", sr.ReadLine()) == 0)
                             {
-                                while (sr.Peek() >= 0) 
+                                while (sr.Peek() >= 0 ) 
                                 {   //TODO выбрать номер работы
 
-                                    text.Append(sr.ReadLine());
+                                    text.Append(line = sr.ReadLine());                                                                    
 
-                                    if (string.Compare("end;", sr.ReadLine()) == 0)
+                                if (string.Compare("end;", line) == 0)
                                     {
                                         break;
                                     }
 
                                 }
+
                             }
                         }
                         richTextBox2.AppendText(text.ToString());
@@ -349,6 +375,7 @@ namespace Shool2
                     Console.WriteLine(ex);   //TODO сохранять логи ошибок в файл
                 }
             }
+
         
     }
 }
